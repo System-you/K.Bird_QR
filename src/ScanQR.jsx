8d5e902fbac4ae9db34002261a6f246c5a74a32a
Jow12560 <jowrock555@gmail.com>
2024-07-15 01:23:05 +0700

    link to text

--- src/ScanQR.jsx
+++ src/ScanQR.jsx
@@ -1,17 +1,16 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Html5QrcodeScanner } from "html5-qrcode";
 
 const Page = () => {
+  const [scannedText, setScannedText] = useState("");
+
   useEffect(() => {
     function onScanSuccess(decodedText, decodedResult) {
-      // handle the scanned code as you like, for example:
-      window.location.assign(decodedText)
+      setScannedText(decodedText);
     }
 
     function onScanFailure(error) {
-      // handle scan failure, usually better to ignore and keep scanning.
-      // for example:
       console.warn(`Code scan error = ${error}`);
     }
 
@@ -29,13 +28,15 @@ const Page = () => {
 
     htmlScanner.render(onScanSuccess, onScanFailure);
 
-    return ()=>{
-        htmlScanner.clear();
-    }
+    return () => {
+      htmlScanner.clear();
+    };
   }, []);
+
   return (
     <div className="w-full h-svh flex flex-col items-center justify-self-center">
       <div id="reader" className="w-[600px]"></div>
+      {scannedText && <p>Scanned Text: {scannedText}</p>}
     </div>
   );
 };
